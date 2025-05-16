6f34cbd2883087baca301ea36e419316befd6521
Mcanita <albadebiase@gmail.com>
2025-05-16 03:24:15 -0300

    Se agregan los componentes del Panel EliminarPersonas

--- src/presentacion/vista/PanelEliminarPersonas.java
+++ src/presentacion/vista/PanelEliminarPersonas.java
@@ -3,10 +3,36 @@ package presentacion.vista;
 import javax.swing.DefaultListModel;
 import javax.swing.JPanel;
 import entidad.Persona;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JList;
 
 public class PanelEliminarPersonas extends JPanel{
+	
+	private JLabel lblTitulo;
+	private JButton btnEliminar;
+	private JList listUsuarios;
 
 	public PanelEliminarPersonas(DefaultListModel<Persona> modeloPersonas) {
+		setLayout(null);
+		
+		lblTitulo = new JLabel("Eliminar usuarios");
+		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblTitulo.setBounds(146, 49, 132, 14);
+		add(lblTitulo);
+		
+		btnEliminar = new JButton("Eliminar");
+		btnEliminar.setBounds(146, 225, 107, 23);
+		add(btnEliminar);
+		
+		listUsuarios = new JList();
+		listUsuarios.setBounds(128, 74, 150, 140);
+		add(listUsuarios);
+		
         // Panel vacio
     }
 }	
+
+
